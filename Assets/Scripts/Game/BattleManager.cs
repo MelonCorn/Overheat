02c67d662543242847eb3709b4e8468a03e0ed5f
MelonCorn <rhfoem456@gmail.com>
2026-01-15 17:45:25 +0900

    [Feat] BattleManager
    전투 진행 관련 매니저 틀

---
+++ Assets/Scripts/Game/BattleManager.cs
@@ -0,0 +1,116 @@
+using Photon.Pun;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleManager : MonoBehaviourPun, IPunObservable
+{
+    public static BattleManager Instance;
+
+    [Header("스테이지 길이 설정")]
+    [SerializeField] float _baseDistance = 1000f;    // 기본 거리
+    [SerializeField] float _distancePerLevel = 200f; // 레벨당 추가 거리
+
+    [Header("스테이지 진행률 슬라이더")]
+    [SerializeField] Slider _progressSlider;
+
+    [Header("날짜 텍스트")]
+    [SerializeField] TextMeshProUGUI _daysText;
+
+    private float _totalDistance;      // 목표 거리         (클리어 목표)
+    private float _currentDistance;    // 현재 이동 거리
+
+    private bool _isStageClear = false; // 스테이지 클리어 여부
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        // 스테이지 목표 설정 (테스트용 1레벨)
+        int stageLevel = 1;
+        _totalDistance = _baseDistance + ((stageLevel - 1) * _distancePerLevel);
+
+        // 이동 거리 초기화
+        _currentDistance = 0f;
+
+        // UI 초기화
+        if (_progressSlider) _progressSlider.value = 0;
+    }
+
+    private void Update()
+    {
+        // 클리어 시 무시
+        if (_isStageClear == true) return;
+
+        // 열차 매니저 없거나, 준비가 덜되었다면 무시
+        if (TrainManager.Instance == null || TrainManager.Instance.IsTrainReady == false) return;
+
+        // 거리 계산
+        if (PhotonNetwork.IsMasterClient)
+        {
+            CalculateProgress();
+        }
+
+        // 진행 UI 갱신
+        UpdateProgressUI();
+    }
+
+    // 거리 계산
+    private void CalculateProgress()
+    {
+        // 엔진 가져와서
+        EngineNode engine = TrainManager.Instance.MainEngine;
+
+        //엔진 있으면
+        if (engine != null)
+        {
+            // 거리 = 속도 * 시간
+            float speed = engine.CurrentSpeed;
+            _currentDistance += speed * Time.deltaTime;
+
+            // 골인 체크
+            if (_currentDistance >= _totalDistance)
+            {
+                ClearStage();
+            }
+        }
+    }
+
+    // 진행도 갱신
+    private void UpdateProgressUI()
+    {
+        // 거리 비율 계산
+        float ratio = _currentDistance / _totalDistance;
+
+        if (_progressSlider != null)
+        {
+            _progressSlider.value = Mathf.Lerp(0, 1, ratio);
+        }
+    }
+
+    // 스테이지 클리어 -> 상점으로 이동
+    private void ClearStage()
+    {
+        if (_isStageClear) return;
+        _isStageClear = true;
+
+        Debug.Log("스테이지 클리어!");
+        PhotonNetwork.LoadLevel("Shop");
+    }
+
+    // 데이터 동기화
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(_currentDistance);
+        }
+        else
+        {
+            _currentDistance = (float)stream.ReceiveNext();
+        }
+    }
+}
