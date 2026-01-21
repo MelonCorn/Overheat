d5000f8c5610f9c222ddf3754c9276c806dc00fc
MelonCorn <rhfoem456@gmail.com>
2026-01-21 15:42:29 +0900

    [Feat] Dispenser DisplayName
    디스펜서 아이템 표시 이름 설정

--- Assets/Scripts/Item/ItemDispenser.cs
+++ Assets/Scripts/Item/ItemDispenser.cs
@@ -4,15 +4,24 @@ using Photon.Pun;
 public class ItemDispenser : MonoBehaviour, IInteractable
 {
     [Header("공급할 아이템")]
-    [SerializeField] string _targetItemName = "Coal";   // 일단 석탄 포대 밖에 없어서 기본은 Fuel
+    [SerializeField] string _targetItemName = "Coal";   // 일단 석탄 포대 밖에 없어서 기본은 Coal
+
+    private string _targetDisplayName;  // 표시 이름 (한글)
+
+    private void Start()
+    {
+        // 화면에 표시될 이름 설정
+        if (ItemManager.Instance.ItemDict.TryGetValue(_targetItemName, out ShopItem itemData))
+            _targetDisplayName = itemData.displayName;
+    }
 
     public string GetInteractText(out bool canInteract)
     {
         canInteract = true;
 
-        return $"{_targetItemName} 획득";
+        return $"{_targetDisplayName} 획득";
     }
-
+                    
     public void OnInteract()
     {
         Debug.Log("아이템 수급 시도");
