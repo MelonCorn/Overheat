f121c27355fa798a4a35266ae56de581f7d50ca3
MelonCorn <rhfoem456@gmail.com>
2026-01-26 14:31:52 +0900

    [Feat] Timeline Manager
    시네머신 타임라인 재생용

---
+++ Assets/Scripts/TimelineManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.Playables;
+
+
+public enum TimelineType // 타임라인 종류
+{
+    None,
+    Start,              // 대기실, 상점 -> 인게임 (출발)
+    GameClear,          // 인게임 -> 상점        (생존)
+    ShopArrival,        // 상점 도착             (도착)
+    GameOver            // 엔진 파괴             (게임오버)
+}
+
+public class TimelineManager : MonoBehaviour
+{
+    [Header("타임라인")]
+    [SerializeField] PlayableDirector _startDirector;       // 출발     (대기실/상점)
+    [SerializeField] PlayableDirector _clearDirector;       // 클리어   (인게임)
+    [SerializeField] PlayableDirector _gameOverDirector;    // 게임오버 (인게임)
+    [SerializeField] PlayableDirector _arrivalDirector;     // 도착     (상점)
+}
