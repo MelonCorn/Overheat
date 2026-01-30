78e38af2ef402afde896bf5c1bfd84e76f3b283a
MelonCorn <rhfoem456@gmail.com>
2026-01-31 00:51:08 +0900

    [Feat] Playe Spawner Intarval
    플레이어 생성 간격

--- Assets/Scripts/Game/PlayerSpawner.cs
+++ Assets/Scripts/Game/PlayerSpawner.cs
@@ -7,30 +7,7 @@ public class PlayerSpawner : MonoBehaviourPunCallbacks
 {
     [SerializeField] GameObject _playerPrefab;
     [SerializeField] Transform _spawnPoint;
-
-    //private IEnumerator Start()
-    //{
-    //    // 네트워크 준비될 때 까지 대기
-    //    while (PhotonNetwork.IsConnectedAndReady == false)
-    //    {
-    //        yield return null;
-    //    }
-    //
-    //    // 프로퍼티 사용하지 않고 조용히 가져오기
-    //    TrainManager trainManager = FindAnyObjectByType<TrainManager>();
-    //
-    //    // 대기실에서 스킵
-    //    // 열차 매니저 준비 완료할 때까지 대기
-    //    // TrainManager가 존재하고, Ready될 때 까지
-    //    if (trainManager != null)
-    //    {
-    //        // 열차 생성 완료될 때까지 대기
-    //        yield return new WaitUntil(() => TrainManager.Instance.IsTrainReady);
-    //    }
-    //
-    //    // 객체 스폰해서 로컬 플레이어로 지정
-    //    PlayerHandler.localPlayer = PhotonNetwork.Instantiate(_playerPrefab.name, _spawnPoint.position, Quaternion.identity).GetComponent<PlayerHandler>();
-    //}
+    [SerializeField] float interval = 1.5f; // 간격
 
     // 플레이어 스폰
     public void PlayerSpawn()
@@ -49,8 +26,17 @@ public class PlayerSpawner : MonoBehaviourPunCallbacks
         // 이미 내 캐릭터가 있다면 중복 생성 방지
         if (PlayerHandler.localPlayer != null) yield break;
 
+        // 내 네트워크 번호
+        int actorNumber = PhotonNetwork.LocalPlayer.ActorNumber;
+
+        // 번호만큼 간격 띄워서
+        Vector3 offset = new Vector3(0f, 0f, (actorNumber - 1) * interval);
+
+        // 최종 스폰 위치
+        Vector3 finalPos = _spawnPoint.position + offset;
+
         // 객체 스폰
-        GameObject player = PhotonNetwork.Instantiate(_playerPrefab.name, _spawnPoint.position, Quaternion.identity);
+        GameObject player = PhotonNetwork.Instantiate(_playerPrefab.name, finalPos, Quaternion.identity);
         PlayerHandler.localPlayer = player.GetComponent<PlayerHandler>();
 
         Debug.Log("로컬 플레이어 스폰 완료");
