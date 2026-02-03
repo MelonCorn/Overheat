75c550ad3d90777aef18e482fd7b94c2750eec59
MelonCorn <rhfoem456@gmail.com>
2026-02-04 01:43:30 +0900

    [Feat] Title Lobby Setting
    타이틀 겸 로비 세팅

--- Assets/Scripts/Lobby/LobbyConnector.cs
+++ Assets/Scripts/Lobby/LobbyConnector.cs
@@ -7,6 +7,9 @@ public class LobbyConnector : MonoBehaviourPunCallbacks
 {
     [SerializeField] Button _joinButton;
     [SerializeField] Button _settingButton;
+    [SerializeField] Button _applyButton;
+
+    [SerializeField] GameObject _loadingUI;
 
     private void Awake()
     {
@@ -41,7 +44,17 @@ public class LobbyConnector : MonoBehaviourPunCallbacks
         if(SettingManager.Instance != null)
         {
             // 세팅 버튼에 설정 패널 열기 설정
-            _settingButton.onClick.AddListener(SettingManager.Instance.OpenSetting);
+            _settingButton.onClick.AddListener( () =>
+            {
+                if (SoundManager.Instance != null) SoundManager.Instance.PlaySFX(SFXType.Click);
+                SettingManager.Instance.OpenSetting();
+            } );
+
+            if(_applyButton != null)
+                _applyButton.onClick.AddListener(() =>
+                {
+                    if (SoundManager.Instance != null) SoundManager.Instance.PlaySFX(SFXType.PopupClose);
+                });
         }
 
         // 페이드 인
@@ -69,15 +82,54 @@ public class LobbyConnector : MonoBehaviourPunCallbacks
 
     public void ClickJoin()
     {
-        PhotonNetwork.JoinRandomOrCreateRoom();
+        // 소리 재생
+        if (SoundManager.Instance != null) SoundManager.Instance.PlaySFX(SFXType.Click);
+
+        // 로딩 UI
+        if (_loadingUI != null) _loadingUI.SetActive(true);
+
+        // 버튼 비활성화
+        if (_joinButton != null) _joinButton.interactable = false;
+
+        // 최대 인원 4명 설정
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 4; 
+
+        // 매칭 속성 필터, 최대인원 필터, 매칭 모드, 로비, SQL필터, 방 이름, 방 옵션
+        PhotonNetwork.JoinRandomOrCreateRoom(null, 0, MatchmakingMode.FillRoom, null, null, null, roomOptions);
     }
 
     public override void OnJoinedRoom()
     {
         if (LoadingManager.Instance != null)
         {
+            // 소리 재생
+            if (SoundManager.Instance != null) SoundManager.Instance.PlaySFX(SFXType.PopupClose);
+
             LoadingManager.Instance.RequestLoadScene("Room");
         }
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        ResetLobby();
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        ResetLobby();
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        ResetLobby();
+    }
+
+    // UI 초기화
+    private void ResetLobby()
+    {
+        if (_loadingUI != null) _loadingUI.SetActive(false);
+        if (_joinButton != null) _joinButton.interactable = true;
     }
 
     // 게임 종료 버튼
